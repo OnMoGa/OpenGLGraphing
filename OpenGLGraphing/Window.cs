8317fa8096d63ffd95b853653c141a1adfd7da8a
Michael Hamilton <micky.hamilton@outlook.com>
2020-08-19 15:13:55 +1000

    Add project files.

---
+++ OpenGLGraphing/Window.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using OpenTK;
+using OpenTK.Graphics;
+
+namespace OpenGLGraphing {
+	public class Window : GameWindow {
+
+		public Window(int width, int height, string title) : base(width, height, GraphicsMode.Default, title) { }
+
+
+
+	}
+}
