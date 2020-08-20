9c48745367ddc18b57301aced5c208f5e7692b13
Michael Hamilton <micky.hamilton@outlook.com>
2020-08-20 18:32:45 +1000

    Showing a window with a triangle

---
+++ OpenGLGraphing/Graph.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenGLGraphing {
+	abstract class Graph {
+
+		public abstract void showInNewWindow(int x, int y, string title);
+	}
+}
--- OpenGLGraphing/Window.cs
+++ OpenGLGraphing/Window.cs
@@ -1,13 +1,102 @@
 ﻿using System;
 using OpenTK;
 using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Input;
 
 namespace OpenGLGraphing {
 	public class Window : GameWindow {
 
-		public Window(int width, int height, string title) : base(width, height, GraphicsMode.Default, title) { }
+		int VertexBufferObject;
+		int VertexArrayObject;
+		Shader shader;
+
+		public Window(int width, int height, string title) : base(width, height, GraphicsMode.Default, title) {
+
+		}
+
+		protected override void OnLoad(EventArgs e)
+		{
+			shader = new Shader("shader.vert", "shader.frag");
+			GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+			VertexArrayObject = GL.GenVertexArray();
+
+			VertexBufferObject = GL.GenBuffer();
+
+			float[] vertices = {
+				-0.5f, -0.5f, 0.0f, //Bottom-left vertex
+				0.5f, -0.5f, 0.0f,  //Bottom-right vertex
+				0.0f,  0.5f, 0.0f   //Top vertex
+			};
+
+			GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+			GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+
+			GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+			GL.EnableVertexAttribArray(0);
+
+			shader.Use();
+
+			GL.BindVertexArray(VertexArrayObject);
+
+			GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+			GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+
+			GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+			GL.EnableVertexAttribArray(0);
+			
+
+			base.OnLoad(e);
+		}
+
+		protected override void OnUnload(EventArgs e)
+		{
+			GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+			GL.DeleteBuffer(VertexBufferObject);
+
+			GL.BindVertexArray(VertexArrayObject);
+
+			shader.Dispose();
+			base.OnUnload(e);
+		}
+
+
+		protected override void OnUpdateFrame(FrameEventArgs e) {
+
+			KeyboardState input = Keyboard.GetState();
+
+			if (input.IsKeyDown(Key.Escape))
+			{
+				Exit();
+			}
+
+			base.OnUpdateFrame(e);
+		}
+
+
+		protected override void OnRenderFrame(FrameEventArgs e)
+		{
+			GL.Clear(ClearBufferMask.ColorBufferBit);
+
+
+			GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+
+			
+			Context.SwapBuffers();
+			base.OnRenderFrame(e);
+		}
+
+
+		protected override void OnResize(EventArgs e)
+		{
+			GL.Viewport(0, 0, Width, Height);
+			base.OnResize(e);
+		}
 
 
 
+
+		
 	}
 }
